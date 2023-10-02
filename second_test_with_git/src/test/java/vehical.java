2e3081119c0f39fd37d38367543ceafb145a1402
ahmadziaei <ahmadziaei50@gmail.com>
2023-10-02 20:44:02 +0330

    third edition

---
+++ second_test_with_git/src/test/java/vehical.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLOutput;
+
+public class vehical{
+
+    String name = "Black";
+
+    @Test
+    void turnOn() {
+        System.out.println("the car has turned on");
+    }
+}
