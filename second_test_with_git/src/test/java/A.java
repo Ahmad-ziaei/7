bda72307975fc5d3e4783d0c23150e268f27e5b5
ahmadziaei <ahmadziaei50@gmail.com>
2023-10-02 20:48:20 +0330

    the first method.

---
+++ second_test_with_git/src/test/java/A.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class A {
+
+    @Test
+    void phone() {
+        System.out.println("Welcom to the java test");
+    }
+}
--- second_test_with_git/src/test/java/vehical.java
+++ second_test_with_git/src/test/java/vehical.java
@@ -1,13 +1,2 @@
-import org.junit.jupiter.api.Test;
 
-import java.sql.SQLOutput;
 
-public class vehical{
-
-    String name = "Black";
-
-    @Test
-    void turnOn() {
-        System.out.println("the car has turned on");
-    }
-}
